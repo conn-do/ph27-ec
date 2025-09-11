14d3ffb2ebec9a926788802bc062dc4cd3433336
LEEHOITUNG <leehoitung87@gmail.com>
2025-09-11 15:55:40 +0800

    changed some layout of the site

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,24 +3,25 @@
 @section('content')
     <h2>商品一覧</h2>
 
-    <ul class="grid grid-cols-3 gap-4">
+    <ul class="grid grid-cols-3 gap-4 mt-8 mb-8">
         @foreach ($products as $product)
-            <li>
-                <a href="{{ route('products.show', ['id' => $product->id]) }}">
+            <li class="product-item">
+                <a href="{{ route('products.show', ['id' => $product->id]) }}" class="product-link">
                     <img src="{{ $product->image }}" alt="{{ $product->name }}">
-                    {{ $product->name }}
+                    <p class="product-info">{{ $product->name }}<br>{{ $product->price }} 円</p>
                 </a>
-                {{ $product->price }} 円
             </li>
         @endforeach
     </ul>
 
-    <h2>セール中の商品</h2>
-    <ul>
+    <h2 class="text-red-500">セール中の商品</h2>
+    <ul class="grid grid-cols-3 gap-4 mt-8 mb-8 text-red-500">
         @foreach ($saleProducts as $product)
-            <li>
-                {{ $product->name }}
-                {{ $product->price }} 円
+            <li class="product-item">
+                <a href="{{ route('products.show', ['id' => $product->id]) }}" class="product-link">
+                    <img src="{{ $product->image }}" alt="{{ $product->name }}">
+                    <p class="product-info">{{ $product->name }}<br>{{ $product->price }} 円</p>
+                </a>
             </li>
         @endforeach
     </ul>
