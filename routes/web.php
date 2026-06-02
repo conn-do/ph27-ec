91de65002b9ddd52f4ba57296bed6e3bc9d9c662
学校用 <Bui@hans.local>
2026-06-02 12:58:22 +0900

    商品一覧とProductモデル作成

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\ProductController;
 
-Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+// Route::inertia('/', 'welcome', [
+//     'canRegister' => Features::enabled(Features::registration()),
+// ])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
@@ -18,7 +19,13 @@ Route::get(
     '/chirps',
     [ChirpController::class, 'index']
 );
+
 Route::post(
     '/chirps',
     [ChirpController::class, 'store']
+);
+
+Route::get(
+    '/',
+    [ProductController::class, 'index']
 );
