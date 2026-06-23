ba5ccc504c326fd5fbf87774ce4ce4fea8a997db
conn-do <kondo@japaan.jp>
2026-06-23 14:02:38 +0900

    注文データの Seeder を作成

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -4,6 +4,8 @@
 
 @section('content')
     <h1>注文ID: {{ $order->id }}</h1>
+    <p>注文日時: {{ $order->created_at->format('Y/m/d H:i') }}</p>
+    <p>金額: {{ number_format($order->total_price) }}円</p>
     <table>
         @foreach ($order->details as $detail)
             <tr>
