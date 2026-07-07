abc2131e58a6062426cc8d89a4c97c0835d1a9ec
bungou0630-byte <bungou.0630@gmail.com>
2026-07-07 13:28:48 +0900

    ニュース機能（作成途中）

--- database/seeders/NewsSeeder.php
+++ database/seeders/NewsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\News;
 
 class NewsSeeder extends Seeder
 {
@@ -11,7 +12,20 @@ class NewsSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        //
-    }
+{
+    News::create([
+        'title' => '1件目のお知らせ',
+        'content' => '<p>テストです</p>',
+    ]);
+
+    News::create([
+        'title' => '2件目のお知らせ',
+        'content' => '<p>テスト2</p>',
+    ]);
+
+    News::create([
+        'title' => '3件目のお知らせ',
+        'content' => '<p>テスト3</p>',
+    ]);
+}
 }
